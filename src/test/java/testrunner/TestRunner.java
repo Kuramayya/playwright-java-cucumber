c29a3e2a64749ef0347d82bf1ecefbfe81ff5a40
Kuramayya <kuramayyak@gmail.com>
2024-11-20 23:54:43 -0500

    ci pipeline code

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         // Rerun failed tests from rerun.txt file
         features = {"src/test/resources/features/"},
         glue = {"stepdefinitions", "hooks"},
-        tags = "@register",
+        tags = "@smoke",
         plugin = {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/rerun.txt"  // Save Failed test scenarios in rerun.txt file
